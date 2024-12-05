67ca03b1f3291f10093eebc56578f73e7bba5f13
alumne <joanchorto1@iesebre.com>
2024-12-05 10:59:24 +0100

    Arreglar la distribucio del blog

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -17,8 +17,10 @@ import IconPhone from "@/components/icons/IconPhone.vue";
   </div>
   <div class="flex flex-col md:flex-row lg:flex-row  items-start  justify-center pt-10 pb-10">
 
+    <div class="flex flex-col w-4/5 space-y-6 items-center justify-center">
+
 <!--    3r mes-->
-    <div class="rounded-lg shadow-lg bg-gray-100 w-4/5 p-10 text-gray-50 text-start space-y-6">
+    <div class="rounded-lg shadow-lg bg-gray-50 w-4/5 p-10 text-gray-50 text-start space-y-6">
       <h2 class="text-3xl font-bold text-stone-800">
         <strong class="text-amber-500 font-bold text-4xl">- </strong> Detectives Privados en Lleida
       </h2>
@@ -30,7 +32,7 @@ import IconPhone from "@/components/icons/IconPhone.vue";
       </router-link>
     </div>
 
-    <div class="rounded-lg shadow-lg bg-gray-100 w-4/5 p-10 text-gray-50 text-start space-y-6">
+    <div class="rounded-lg shadow-lg bg-gray-50 w-4/5 p-10 text-gray-50 text-start space-y-6">
       <h2 class="text-3xl font-bold text-stone-800">
         <strong class="text-amber-500 font-bold text-4xl">- </strong> Servicios de Investigación Privada en Tarragona
       </h2>
@@ -42,7 +44,7 @@ import IconPhone from "@/components/icons/IconPhone.vue";
       </router-link>
     </div>
 
-    <div class="rounded-lg shadow-lg bg-gray-100 w-4/5 p-10 text-gray-50 text-start space-y-6">
+    <div class="rounded-lg shadow-lg bg-gray-50 w-4/5 p-10 text-gray-50 text-start space-y-6">
       <h2 class="text-3xl font-bold text-stone-800">
         <strong class="text-amber-500 font-bold text-4xl">- </strong> Detectives Privados en Lleida
       </h2>
@@ -57,27 +59,27 @@ import IconPhone from "@/components/icons/IconPhone.vue";
 
 
 <!--    2n mes-->
-    <div class="rounded-lg shadow-lg bg-gray-100 w-4/5 p-10 text-gray-50 text-start space-y-6">
+    <div class="rounded-lg shadow-lg bg-gray-50 w-4/5 p-10 text-gray-50 text-start space-y-6">
       <h2 class="text-3xl font-bold text-stone-800"><strong class="text-amber-500 font-bold text-4xl">- </strong> Detectives Privados: Investigación de Bajas Laborales Fingidas</h2>
       <p class="text-lg pb-6 text-stone-800">Descubre cómo los detectives privados en Terres de l'Ebre investigan bajas laborales fingidas con métodos modernos y discretos, ayudando a empresas y particulares a obtener las pruebas necesarias.</p>
       <router-link to="/baja-laboral-fingida" class="bg-amber-500 text-gray-50  text-lg p-2 rounded-md hover:bg-amber-500 hover:text-gray-50">Llegir més</router-link>
     </div>
-    <div class="rounded-lg shadow-lg bg-gray-100 w-4/5 p-10 text-gray-50 text-start space-y-6">
+    <div class="rounded-lg shadow-lg bg-gray-50 w-4/5 p-10 text-gray-50 text-start space-y-6">
       <h2 class="text-3xl font-bold text-stone-800"><strong class="text-amber-500 font-bold text-4xl">- </strong> Investigaciones Privadas en Cataluña y Tarragona</h2>
       <p class="text-lg pb-6 text-stone-800">En Cataluña y Tarragona, los detectives privados trabajan en una amplia gama de investigaciones, ofreciendo soluciones personalizadas para descubrir la verdad en casos complejos.</p>
       <router-link to="/investigador-privado-cataluña-tarragona" class="bg-amber-500 text-gray-50  text-lg p-2 rounded-md hover:bg-amber-500 hover:text-gray-50">Llegir més</router-link>
     </div>
-    <div class="rounded-lg shadow-lg bg-gray-100 w-4/5 p-10 text-gray-50 text-start space-y-6">
+    <div class="rounded-lg shadow-lg bg-gray-50 w-4/5 p-10 text-gray-50 text-start space-y-6">
       <h2 class="text-3xl font-bold text-stone-800"><strong class="text-amber-500 font-bold text-4xl">- </strong> Investigadores Privados en Tortosa</h2>
       <p class="text-lg pb-6 text-stone-800">En Tortosa, los detectives privados son expertos en resolver casos delicados, ofreciendo servicios confiables para clientes que necesitan respuestas claras y precisas.</p>
       <router-link to="/investigador-privado-tortosa" class="bg-amber-500 text-gray-50  text-lg p-2 rounded-md hover:bg-amber-500 hover:text-gray-50">Llegir més</router-link>
     </div>
-    <div class="rounded-lg shadow-lg bg-gray-100 w-4/5 p-10 text-gray-50 text-start space-y-6">
+    <div class="rounded-lg shadow-lg bg-gray-50 w-4/5 p-10 text-gray-50 text-start space-y-6">
       <h2 class="text-3xl font-bold text-stone-800"><strong class="text-amber-500 font-bold text-4xl">- </strong> El Mejor Detective Privado en Tarragona 2024</h2>
       <p class="text-lg pb-6 text-stone-800">Descubre quién es reconocido como el mejor detective privado en Tarragona en 2024, destacado por su profesionalismo, discreción y resultados excepcionales en investigaciones complejas.</p>
       <router-link to="/mejor-detective-privado-tarragona-2024" class="bg-amber-500 text-gray-50 text-lg p-2 rounded-md hover:bg-amber-500 hover:text-gray-50">Llegir més</router-link>
     </div>
-    <div class="rounded-lg shadow-lg bg-gray-100 w-4/5 p-10 text-gray-50 text-start space-y-6">
+    <div class="rounded-lg shadow-lg bg-gray-50 w-4/5 p-10 text-gray-50 text-start space-y-6">
       <h2 class="text-3xl font-bold text-stone-800"><strong class="text-amber-500 font-bold text-4xl">- </strong> Cómo Contratar un Detective Privado en Tarragona</h2>
       <p class="text-lg pb-6 text-stone-800">Conoce los pasos clave para contratar un detective privado en Tarragona y cómo garantizar que tu elección sea profesional y confiable para tus necesidades específicas.</p>
       <router-link to="/como-contratar-un-detective-privado-tarragona" class="bg-amber-500 text-gray-50 text-lg p-2 rounded-md hover:bg-amber-500 hover:text-gray-50">Llegir més</router-link>
@@ -88,45 +90,45 @@ import IconPhone from "@/components/icons/IconPhone.vue";
 
 
     <!--   1r mes -->
-    <div class="flex flex-col lg:w-4/5 w-full md:w-4/5 items-center space-y-6 justify-center">
-      <div class="rounded-lg shadow-lg bg-gray-100 w-4/5 p-10 text-gray-50 text-start space-y-6">
+      <div class="rounded-lg shadow-lg bg-gray-50 w-4/5 p-10 text-gray-50 text-start space-y-6">
         <h2 class="text-3xl font-bold text-stone-800"><strong class="text-amber-500 font-bold text-4xl">- </strong> Descifrando Misterios: La Importancia del Detective Privado en Tarragona</h2>
         <p class="text-lg pb-6 text-stone-800 ">En el corazón de Tarragona, donde el Mediterráneo susurra secretos y las calles cuentan historias entrelazadas, la necesidad de un detective privado se vuelve una realidad palpable. ¿Qué papel juegan estos investigadores en la trama de esta ciudad llena de encanto y enigmas? </p>
         <router-link to="/detectiu-privat-tarragona" class="bg-amber-500 text-gray-50  text-lg p-2 rounded-md hover:bg-amber-500 hover:text-gray-50">Llegir més</router-link>
       </div>
-      <div class="rounded-lg shadow-lg bg-gray-100 w-4/5 p-10 text-gray-50 text-start space-y-6">
+      <div class="rounded-lg shadow-lg bg-gray-50 w-4/5 p-10 text-gray-50 text-start space-y-6">
         <h2 class="text-3xl font-bold text-stone-800"><strong class="text-amber-500 font-bold text-4xl">- </strong> Descubriendo la Verdad: Detective de Infidelidades en Castellón</h2>
         <p class="text-lg pb-6 text-stone-800 ">En un mundo donde las relaciones personales pueden ser complicadas y los secretos a menudo se esconden en las sombras, contar con la ayuda de un detective de infidelidades en Castellón puede ser la clave para desentrañar la verdad oculta. </p>
         <router-link to="/detectiu-infidelitat-castellon" class="bg-amber-500 text-gray-50  text-lg p-2 rounded-md hover:bg-amber-500 hover:text-gray-50">Llegir més</router-link>
       </div>
-      <div class="rounded-lg shadow-lg bg-gray-100 w-4/5 p-10 text-gray-50 text-start space-y-6">
+      <div class="rounded-lg shadow-lg bg-gray-50 w-4/5 p-10 text-gray-50 text-start space-y-6">
         <h2 class="text-3xl font-bold text-stone-800"><strong class="text-amber-500 font-bold text-4xl">- </strong> Desvelando Misterios: Detectives Privados en Terres de l'Ebre</h2>
         <p class="text-lg pb-6 text-stone-800 ">En las tierras que baña el río Ebro, donde la historia se entrelaza con la modernidad y los secretos yacen entre los recodos de la naturaleza, la presencia de detectives privados en Terres de l'Ebre se convierte en un faro de esperanza para aquellos que buscan respuestas en medio de la incertidumbre.</p>
         <router-link to="/detectiu-privat-terres-de-lebre" class="bg-amber-500 text-gray-50  text-lg p-2 rounded-md hover:bg-amber-500 hover:text-gray-50">Llegir més</router-link>
       </div>
 
-
     </div>
+
     <div class="md:visible lg:visible invisible flex flex-col w-1/5 border-l-4 border-amber-500 p-2 items-start justify-start">
-        <ul class="">
-          <li class="flex items-center content-center">
-            <icon-mail class="fill-amber-500 size-20"/>
-            <p>info@detfrau.com</p>
-          </li>
-          <li class="flex items-center content-center">
-            <icon-phone class="fill-amber-500 size-20"/>
-            <p>643 69 19 72</p>
-          </li>
-          <li class="flex items-center text-center  my-7 justify-center content-center">
-            <router-link to="/contacte-detectiu-privat" class="hover:p-1 bg-amber-500 text-gray-50 text-lg p-2 rounded-md hover:text-gray-50">Contacta'ns</router-link>
+      <ul class="">
+        <li class="flex items-center content-center">
+          <icon-mail class="fill-amber-500 size-20"/>
+          <p>info@detfrau.com</p>
+        </li>
+        <li class="flex items-center content-center">
+          <icon-phone class="fill-amber-500 size-20"/>
+          <p>643 69 19 72</p>
+        </li>
+        <li class="flex items-center text-center  my-7 justify-center content-center">
+          <router-link to="/contacte-detectiu-privat" class="hover:p-1 bg-amber-500 text-gray-50 text-lg p-2 rounded-md hover:text-gray-50">Contacta'ns</router-link>
 
-          </li>
-        </ul>
+        </li>
+      </ul>
     </div>
+    </div>
+
 
   </div>
 
-</div>
 </template>
 
 <style scoped>
